f5d6134e3e020f9cdf96dc0c33c1fe6cadaa8e44
Johnathan <johnathan.nguyen1999@gmail.com>
2020-07-02 21:14:12 -0400

    working slideshow
    working slideshow needs to fix background

--- script.js
+++ script.js
@@ -1,28 +1,39 @@
-// var slideIndex = 1;
-// showSlides(slideIndex);
+//starting at the first picture to display
+var slideIndex = 1;
+showSlides(slideIndex);
 
-// // Next/previous controls
-// function plusSlides(n) {
-//   showSlides(slideIndex += n);
-// }
+//Next/prev controls
+//n is declared in html
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
 
-// // Thumbnail image controls
-// function currentSlide(n) {
-//   showSlides(slideIndex = n);
-// }
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
 
-// function showSlides(n) {
-//   var i;
-//   var slides = document.getElementsByClassName("mySlides");
-//   var dots = document.getElementsByClassName("dot");
-//   if (n > slides.length) {slideIndex = 1}
-//   if (n < 1) {slideIndex = slides.length}
-//   for (i = 0; i < slides.length; i++) {
-//       slides[i].style.display = "none";
-//   }
-//   for (i = 0; i < dots.length; i++) {
-//       dots[i].className = dots[i].className.replace(" active", "");
-//   }
-//   slides[slideIndex-1].style.display = "block";
-//   dots[slideIndex-1].className += " active";
-// }
+//n is declared in HTML
+function showSlides(n){
+    var i;
+    var slides = document.getElementsByClassName("image");
+    console.log(slides.length)
+    var dots = document.getElementsByClassName("dot");
+    //if on the fourth slide and next gets pushed go back to first slide
+    if(n > slides.length){
+        slideIndex = 1
+    }
+    //if on the first slide and prev gets pushed go to last slide
+    if(n < 1){
+        slideIndex = slideIndex.length;
+    }
+
+    for(i = 0 ; i < slides.length ; i++){
+        slides[i].style.display = "none";
+    }
+    for(i = 0 ; i < dots.length ; i++){
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
